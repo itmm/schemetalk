8dc756453fdcef3c033be9c1acb34ce89828e9e8
Timm Knape <timm@knp.de>
2021-06-12 23:01:45 +0200

    add nil to state

---
+++ cmake-build-debug/node.cpp
@@ -0,0 +1,8 @@
+#include "node.h"
+#include "map.h"
+
+void register_node_entries(Node_Ptr state) {
+	auto s { dynamic_cast<Map *>(state.get()) };
+	if (! s) { fail("no state"); }
+	s->insert("nil", nil);
+}
